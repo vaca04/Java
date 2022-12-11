76324a93006cf7261b4811023761c6de5bdbdc44
Erick Yael Vaca Gorostieta <vacagorostietaerickyael.com>
2022-12-11 16:13:21 -0600

    new file:   Comments/.vscode/settings.json 	new file:   Comments/bin/App.class 	new file:   Comments/code.png 	new file:   Comments/src/Comments.java

---
+++ Comments/src/Comments.java
@@ -0,0 +1,9 @@
+public class Comments {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // this is a line
+        /*
+         * this is a multi line
+         * in line 3 print Hello, World! in a screen
+         */
+    }
+}
