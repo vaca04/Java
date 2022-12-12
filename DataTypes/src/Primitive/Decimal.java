e943d4bbd8ea46f23973ae187f6c1f1019d3f84f
Erick Yael Vaca Gorostieta <vacagorostietaerickyael.com>
2022-12-11 23:05:13 -0600

    new file:   DataTypes/src/Primitive/Decimal.java 	new file:   DataTypes/src/Primitive/Decimal.png

---
+++ DataTypes/src/Primitive/Decimal.java
@@ -0,0 +1,9 @@
+package Primitive;
+
+public class Decimal {
+    public static void main(String[] args) {
+        double doubleDouble = 3.1416;
+        float doubleFloat = 301416f;
+        System.out.println("double:" + doubleDouble + "\nfloat:" + doubleFloat);
+    }
+}
