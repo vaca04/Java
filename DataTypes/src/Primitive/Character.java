cb13a247c5b15d8ad0971a8a55120ad087ee2810
Erick Yael Vaca Gorostieta <vacagorostietaerickyael.com>
2022-12-12 00:08:04 -0600

    new file:   DataTypes/src/Primitive/Character.java 	new file:   DataTypes/src/Primitive/Character.png

---
+++ DataTypes/src/Primitive/Character.java
@@ -0,0 +1,8 @@
+package Primitive;
+
+public class Character {
+    public static void main(String[] args) {
+        char character = 'z';
+        System.out.println("char:" + character);
+    }
+}
