511cd034d48910ce58a4e639c060361292aaa0f3
Erick Yael Vaca Gorostieta <vacagorostietaerickyael.com>
2022-12-18 17:45:14 -0600

    modified:   DataTypes/src/Primitive/Decimal.java

--- DataTypes/src/Primitive/Decimal.java
+++ DataTypes/src/Primitive/Decimal.java
@@ -3,7 +3,7 @@ package Primitive;
 public class Decimal {
     public static void main(String[] args) {
         double doubleDouble = 3.1416;
-        float doubleFloat = 301416f;
+        float doubleFloat = 31416f;
         System.out.println("double:" + doubleDouble + "\nfloat:" + doubleFloat);
     }
 }
