2abd79adc75b8c85bad162b20f78ddb8ec8b3059
Erick Yael Vaca Gorostieta <vacagorostietaerickyael.com>
2022-12-21 11:59:23 -0600

    new file:   DataTypes/src/NotPrimitive/Chain.java 	DataTypes/src/NotPrimitive/code.png

---
+++ DataTypes/src/NotPrimitive/Chain.java
@@ -0,0 +1,9 @@
+package NotPrimitive;
+
+public class Chain {
+    public static void main(String[] args) {
+        String chainNull = null;
+        String chain = "chain";
+        System.out.println("the chain is: " + chain + "\nthe value is: " + chainNull);
+    }
+}
